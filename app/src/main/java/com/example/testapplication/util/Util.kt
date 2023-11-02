0d6e2f54ee2643cbe5a28112e3e2c81bf690496e
Mustafa <musertac@hotmail.com>
2023-11-02 13:59:16 +0300

    change photo

--- app/src/main/java/com/example/testapplication/util/Util.kt
+++ app/src/main/java/com/example/testapplication/util/Util.kt
@@ -2,7 +2,7 @@ package com.example.testapplication.util
 
 object Util {
 
-    const val API_KEY="YOU ENTER İT"
+    const val API_KEY="40431400-5b7ec9cacc71eb0c1e13ddb14"
     const val BASE_URL = "https://pixabay.com"
 
 }
